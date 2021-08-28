3ad58eea1ed7301b966a68b92a07ec5da98028cf
cituk <programaciondyn3.mx@sandos.com>
2021-08-27 22:47:01 -0500

    Promesas

--- 01-bases-js/src/index.js
+++ 01-bases-js/src/index.js
@@ -1,10 +1,22 @@
 
+console.log('Inicio!!')
+
+//promesa , resolve si todo sale bien, reject si sale algun error
+//resolve y reject son funciones
+// el then es para indicar algo cuando se hace correctamente la promesa y luego haz lo que sigue
+//then recibe un argumento que es opcional, que es todo lo que la promesa resuelva
+//.catch se ocupa para atrapar el error en una promesa
+new Promise( (resolve, reject) => {
+    console.log('cuerpo de la promesa')
+    // resolve('Promesa resuelta')
+    reject('Promesa resuelta con error')
+
+}).then( (msg)=> console.log(msg) ) //esta linea se puede reducir así .then(console.log) debido a que por referencia js sabe que solo es un argumento
+.catch( err => console.log(err)) //esta linea se puede reducir así .catch(console.log)
+
+console.log('fin!!')
 
 
-
-
-
-console.log('Hola Mundo!!')
 
 
 
