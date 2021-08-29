0b8d9e80fc0d1719635443fe923668262cb38c04
cituk <programaciondyn3.mx@sandos.com>
2021-08-28 23:35:42 -0500

    Introducción a los eventos

--- 02-bases-vuejs/app.js
+++ 02-bases-vuejs/app.js
@@ -1,12 +1,22 @@
 const app = Vue.createApp({
-    template: `
-        <h1>{{message}}</h1>
-        <p>{{parrafo}}</p>
-    `,
+    // template: `
+    //     <h1>{{message}}</h1>
+    //     <p>{{parrafo}}</p>
+    // `,
     data(){
         return {
-            message: 'Hola mundo',
-            parrafo: 'Desde app.js'
+            quote: 'Soy Batman',
+            author: 'Desde app.js'
+        }
+    },
+    methods:{
+        changeQuote(){
+            console.log('hola mundo de nuevo')
+            this.author = 'Nuevo autor'
+            this.capitalize()
+        },
+        capitalize(){
+            this.quote = this.quote.toUpperCase()
         }
     }
 })
