b1ef07d9e4d482f3650ad27fa799946112a3036e
cituk <programaciondyn3.mx@sandos.com>
2021-08-29 02:22:37 -0500

    Incrementar y Decrementar

--- 03-fundamentos/src/components/Counter.vue
+++ 03-fundamentos/src/components/Counter.vue
@@ -1,11 +1,12 @@
 <template>
-    <h2>Counter</h2>
-    <p>{{counter}}<sup>2</sup>={{squareCounter}}</p>
-    <p>{{counter}}<sup>2</sup>={{squareCounter}}</p>
-    <p>{{counter}}<sup>2</sup>={{squareCounter}}</p>
-    <p>{{counter}}<sup>2</sup>={{squareCounter}}</p>
-    <p>{{counter}}<sup>2</sup>={{squareCounter}}</p>
-
+    <div>
+        <h2>Counter</h2>
+        <p>{{counter}}<sup>2</sup>={{squareCounter}}</p>
+        <div>
+            <button @click="decrease">-1</button> 
+            <button @click="increase">+1</button>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -19,16 +20,37 @@
         computed:{
             squareCounter(){
                 return this.counter * this.counter
-            }
+            },
+            
         },
         methods:{
             getSquareValue(){
                 return this.counter * this.counter
+            },
+            increase(){
+                this.counter += 1
+            },
+            decrease(){
+                this.counter -= 1
             }
         }
     }
 </script>
 
 <style >
+    button{
+        margin-inline-end: 1rem; 
+        background-color: #64BB87;
+        border: 1px solid white;
+        color: white;
+        border-radius: 5px;
+        padding: 5px 15px;
+        cursor: pointer;
+        transition: 0.3s ease-in-out;
+    }
 
+    button:hover{
+        background-color: #5aa67b;        
+        transition: 0.3s ease-in-out;
+    }
 </style>
