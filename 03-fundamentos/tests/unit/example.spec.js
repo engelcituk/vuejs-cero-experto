ef77792d226b057c4c9a37065af2401e514d7495
cituk <programaciondyn3.mx@sandos.com>
2021-08-30 00:05:25 -0500

    Expect

--- 03-fundamentos/tests/unit/example.spec.js
+++ 03-fundamentos/tests/unit/example.spec.js
@@ -1,15 +1,11 @@
 describe('Example Component', ()=> {
   test('Debe de ser mayor a 10', ()=>{
     //Arreglar
-    let value = 5
+    let value = 9
     //estimulo
     value = value + 2
 
     //Obtener el resultado
-    if(value > 10 ){
-
-    } else {
-      throw `${value} no es mayor a 10`
-    }
+    expect(value).toBeGreaterThan(10);
   })
 })
