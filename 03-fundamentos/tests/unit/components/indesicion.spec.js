a14674b4dc62d9db6d2a357228e1e1a9a877d410
cituk <programaciondyn3.mx@sandos.com>
2021-08-31 00:45:39 -0500

    Spy y Mocks

--- 03-fundamentos/tests/unit/components/indesicion.spec.js
+++ 03-fundamentos/tests/unit/components/indesicion.spec.js
@@ -4,18 +4,22 @@ import Indecision from '@/components/Indecision'
 
 describe('Indecision Component', ()=> {
     let wrapper
+    let clgSpy
 
     beforeEach( () => {
         wrapper = shallowMount(Indecision) //monto el counter, en cada test, asi si se modifica algo en este se vuelve a su estado inicial
+        clgSpy = jest.spyOn(console, 'log')
     })
+
     test('Debe de hacer match con el snapshot', () => {
         
         expect( wrapper.html() ).toMatchSnapshot()
     })
 
-    test('Escribir en el input no debe de disparar nada, (console.log())', () => {
-        
-        
+    test('Escribir en el input no debe de disparar nada, (console.log())', async () => {
+        const input = wrapper.find('input')
+        await input.setValue('Hola mundo')
+        expect(clgSpy).toHaveBeenCalledTimes(1)
     })
 
     test('Al escribir el simbolo de "?" debe de disparar el fetch', () => {
