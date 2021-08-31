bde54d0816dd5d98f37b120ce43ec0d5baab4140
cituk <programaciondyn3.mx@sandos.com>
2021-08-31 01:34:43 -0500

    Pruebas sobre Fetch Api

--- 03-fundamentos/tests/unit/components/indesicion.spec.js
+++ 03-fundamentos/tests/unit/components/indesicion.spec.js
@@ -6,6 +6,15 @@ describe('Indecision Component', ()=> {
     let wrapper
     let clgSpy
 
+    // mock de fetch api, genero un mock de fetch por node no lo tiene
+    global.fetch = jest.fn( () => Promise.resolve({
+        json: () => Promise.resolve({
+            answer: 'yes',
+            forced: false,
+            image: 'https://yesno.wtf/assets/yes/2.gif'
+        })
+    }))
+
     beforeEach( () => {
         wrapper = shallowMount(Indecision) //monto el counter, en cada test, asi si se modifica algo en este se vuelve a su estado inicial
         clgSpy = jest.spyOn(console, 'log')
@@ -36,9 +45,15 @@ describe('Indecision Component', ()=> {
         expect(getAnswerSpy).toHaveBeenCalled()  
     })
 
-    test('Pruebas en getAnswer', () => {
-        
-        
+    test('Pruebas en getAnswer', async () => {
+        await wrapper.vm.getAnswer()
+        // console.log(wrapper.vm.img)
+        // console.log(wrapper.vm.answer)
+        const img = wrapper.find('img')
+        expect(img.exists() ).toBeTruthy()
+        expect(wrapper.vm.img ).toBe('https://yesno.wtf/assets/yes/2.gif')
+        expect(wrapper.vm.answer).toBe('yes')
+
     })
 
     test('Pruebas en getAnswer - Fallo en API', () => {
