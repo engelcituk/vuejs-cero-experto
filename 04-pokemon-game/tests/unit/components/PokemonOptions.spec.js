586761a685e4e4f4c08d9946fa11c47388382a09
cituk <programaciondyn3.mx@sandos.com>
2021-09-03 00:41:17 -0500

    Pruebas con emisiones

--- 04-pokemon-game/tests/unit/components/PokemonOptions.spec.js
+++ 04-pokemon-game/tests/unit/components/PokemonOptions.spec.js
@@ -18,4 +18,29 @@ describe('Pokemon Options component', ()=> {
         expect(wrapper.html()).toMatchSnapshot() // toMatchInlineSnapshot
     })
   
+    test('Debe de mostrar las 4 opciones correctamente', ()=> {
+        const [l1, l2, l3, l4] = wrapper.findAll('li')
+        expect( l1.text() ).toBe('bulbasaur')
+        expect( l2.text() ).toBe('ivysaur')
+        expect( l3.text() ).toBe('venusaur')
+        expect( l4.text() ).toBe('charmander')
+
+    })
+
+
+    test('Debe de emitir "selection" con sus respectivos parametros al hacer click', ()=> {
+        const [l1, l2, l3, l4] = wrapper.findAll('li')
+        l1.trigger('click')
+        l2.trigger('click')
+        l3.trigger('click')
+        l4.trigger('click')
+
+        // console.log(wrapper.emitted('selection'))
+        expect( wrapper.emitted('selection').length).toBe(4) // 4 clicks
+        expect( wrapper.emitted('selection').[0]).toEqual([1]) //toBe no funciona con arreglos u objetos
+        expect( wrapper.emitted('selection').[1]).toEqual([2]) //toBe no funciona con arreglos u objetos
+        expect( wrapper.emitted('selection').[2]).toEqual([3]) //toBe no funciona con arreglos u objetos
+        expect( wrapper.emitted('selection').[3]).toEqual([4]) //toBe no funciona con arreglos u objetos
+        
+    })
 })
