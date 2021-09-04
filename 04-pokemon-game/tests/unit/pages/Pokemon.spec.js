7dede21f0cdeb598e2bd8cf7a4c40bf8c4d958da
cituk <programaciondyn3.mx@sandos.com>
2021-09-03 23:18:40 -0500

    Pruebas de que los componentes existan

--- 04-pokemon-game/tests/unit/pages/Pokemon.spec.js
+++ 04-pokemon-game/tests/unit/pages/Pokemon.spec.js
@@ -23,7 +23,7 @@ describe('Pokemon Page component', ()=> {
     })
 
     test('Debe de hacer match con el snapshot cuando cargan los pokemons',  () => {
-        const wrapper = mount( PokemonPage, {
+        const wrapper = shallowMount( PokemonPage, {
             data(){
                 return {
                     pokemonArr: pokemons,
@@ -36,6 +36,30 @@ describe('Pokemon Page component', ()=> {
         })
 
         expect( wrapper.html() ).toMatchSnapshot()
+    })
+
+    test('Debe de mostrar los componentes de PokemonPicture y PokemonOptions',  () => {
+
+        const wrapper = shallowMount( PokemonPage, {
+            data(){
+                return {
+                    pokemonArr: pokemons,
+                    pokemon: pokemons[0],
+                    showPokemon: false,
+                    showAnswer: false,
+                    message: ''
+                }
+            }
+        })
+
+        const picture = wrapper.find('pokemon-picture-stub') 
+        const options = wrapper.find('pokemon-options-stub') 
+
+        expect( picture.exists() ).toBeTruthy()
+        expect( options.exists() ).toBeTruthy()
+
+        expect( picture.attributes('pokemonid') ).toBe('1')
+        expect( options.attributes('pokemons') ).toBeTruthy()
 
     })
   
