0a37a149385c86584d3e18fa7bad6a52409d31b8
cituk <programaciondyn3.mx@sandos.com>
2021-09-05 00:50:28 -0500

    Disparar métodos del ciclo de vida

--- 05-rutas-ciclo/src/modules/pokemon/pages/AboutPage.vue
+++ 05-rutas-ciclo/src/modules/pokemon/pages/AboutPage.vue
@@ -1,3 +1,66 @@
 <template>
-    <h1>About Page</h1>
+    <div>
+        <h1>About Page</h1>
+        <hr>
+        <h2>{{name}}</h2>
+        <button @click="onChangeName">
+            Cambiar Nombre
+        </button>
+    </div>
 </template>
+<script>
+export default {
+    data(){
+        return {
+            name: 'eCituk'
+        }
+    },
+    beforeCreate(){
+        console.log('beforeCreate')
+    },
+    created(){
+        this.name = 'Juan Carlos'
+        console.log('created')
+        //Aquí podemos hacer las peticiones HTTP
+    },
+    beforeMount(){
+        console.log('beforeMount')
+    },
+    mounted(){
+        console.log('mounted')
+    },
+    beforeUpdate(){
+        console.log('beforeUpdate')
+    },
+    updated(){
+        console.log('updated')
+    },
+    activated(){
+        console.log('activated')
+    },
+    deactivated(){
+        console.log('deactivated')
+    },
+    beforeUnmount(){
+        console.log('beforeUnmount')
+    },
+    unmounted(){
+        console.log('unmounted')
+    },
+    errorCaptured(){
+        console.log('errorCaptured')
+    },
+    renderTracked(){
+        console.log('renderTracked')
+    },
+    renderTriggered(){
+        console.log('renderTriggered')
+    },
+    methods:{
+        onChangeName(){
+            console.log('se cambiará nombre')
+            this.name = 'eCituk Caamal'
+        }
+    }
+}
+</script>
