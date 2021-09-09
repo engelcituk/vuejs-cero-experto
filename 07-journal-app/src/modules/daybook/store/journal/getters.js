77ec431c40b1ab77924614d7a3bf383a80078119
cituk <programaciondyn3.mx@sandos.com>
2021-09-09 00:53:14 -0500

    Journal - Vuex Module

---
+++ 07-journal-app/src/modules/daybook/store/journal/getters.js
@@ -0,0 +1,13 @@
+/*
+    export const myGetter =  (state) => {
+        return state.something
+    }
+*/
+
+export const getEntriesByTerm =  (/*state*/) => {
+    // return state.something
+}
+
+export const getEntryById =  (/*state*/) => {
+    // return state.something
+}
---
+++ 07-journal-app/src/modules/daybook/store/journal/mutations.js
@@ -0,0 +1,17 @@
+/*
+    export const myMuttation =  (state) => {
+        return state.something
+    }
+*/
+
+export const setEntries =  (/*state*/) => {
+    // return state.something
+}
+
+export const updateEntry =  (/*state*/) => {
+    // return state.something
+}
+
+export const addEntry =  (/*state*/) => {
+    // return state.something
+}
