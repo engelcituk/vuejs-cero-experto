befae4629bf910d1abd518da8fc0909cb34044e0
cituk <programaciondyn3.mx@sandos.com>
2021-09-09 23:19:23 -0500

    Mostrar entrada en pantalla o redireccionar al usuario

--- 07-journal-app/src/modules/daybook/views/EntryView.vue
+++ 07-journal-app/src/modules/daybook/views/EntryView.vue
@@ -2,9 +2,9 @@
     <div>
         <div class="entry-title d-flex justify-content-between p-2">
             <div>
-                <span class="text-success fs-3 fw-bold">08</span>
-                <span class="mx-1 fs-3">Agosto</span>
-                <span class="mx-2 fs-4 fw-light">2021, Jueves</span>
+                <span class="text-success fs-3 fw-bold">{{day}}</span>
+                <span class="mx-1 fs-3">{{month}}</span>
+                <span class="mx-2 fs-4 fw-light">{{yearDay}}</span>
             </div>
             <div>
                 <button class="btn btn-danger mx-2">
@@ -20,6 +20,7 @@
         <hr>
         <div class="d-flex flex-column px-3 h-75">
             <textarea
+                v-model="entry.text"
                 placeholder="¿Que sucedió hoy?"
             ></textarea>
         </div>
@@ -36,6 +37,7 @@
 <script>
     import { defineAsyncComponent } from 'vue';
     import { mapGetters } from 'vuex'
+    import getDayMonthYear from './../helpers/getDayMonthYear'
 
     export default {
         props:{
@@ -47,17 +49,34 @@
         components:{
             Fab: defineAsyncComponent( ()=> import('./../components/Fab')) //lazy load component
         },
+        data(){
+            return {
+                entry: null
+            }
+        },
         created(){
             this.loadEntry()
         },
         computed:{
             ...mapGetters('journal',['getEntryById']),
-            
+            day(){
+                const { day } = getDayMonthYear(this.entry.date)
+                return day
+            },
+            month(){
+                const { month } = getDayMonthYear(this.entry.date)
+                return month
+            },
+            yearDay(){
+                const { yearDay } = getDayMonthYear(this.entry.date)
+                return yearDay
+            }
         },
         methods:{
             loadEntry(){
                 const entry = this.getEntryById(this.id)
-                console.log(entry)
+                if( !entry ) this.$router.push({name:'no-entry'})
+                this.entry = entry
             }
         }
     }
