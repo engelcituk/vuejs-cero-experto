1eba5ee2f676c1c7056382c1be1732f20f16fe91
cituk <programaciondyn3.mx@sandos.com>
2021-09-10 00:34:43 -0500

    Realizar el commit de una mutación

--- 07-journal-app/src/modules/daybook/store/journal/mutations.js
+++ 07-journal-app/src/modules/daybook/store/journal/mutations.js
@@ -4,8 +4,9 @@
     }
 */
 
-export const setEntries =  (/*state*/) => {
-    // return state.something
+export const setEntries =  (state, entries) => {
+  state.entries = [...state.entries, ...entries] // crear un arreglo con los valoes antiguos y los nuevos entries
+  state.isLoading = false
 }
 
 export const updateEntry =  (/*state*/) => {
--- 07-journal-app/src/modules/daybook/store/journal/state.js
+++ 07-journal-app/src/modules/daybook/store/journal/state.js
@@ -1,23 +1,4 @@
 export default () => ({
     isLoading: true,
-    entries:[
-        {
-            id: '1',
-            date: new Date().toDateString(),
-            text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quaerat ex quasi quam? Assumenda voluptas tenetur tempore incidunt ad consequatur delectus mollitia veritatis ducimus placeat rerum, dignissimos deleniti, officiis cumque!',
-            picture: null
-        },
-        {
-            id: '2' ,
-            date: new Date().toDateString(),
-            text:'Tempor quis excepteur esse elit aliquip et ad anim aliquip veniam consequat nostrud elit qui. Consectetur deserunt do sint incididunt exercitation duis. Fugiat adipisicing in sint occaecat esse. Ea nulla ad proident nulla voluptate elit amet aliquip reprehenderit occaecat id ullamco. Id et magna aute sunt ut anim pariatur culpa commodo est voluptate sint.',
-            picture: null
-        },
-        {
-            id: '3' ,
-            date: new Date().toDateString(),
-            text:'Ex dolore nostrud exercitation occaecat pariatur. Irure esse tempor nulla mollit Lorem quis consectetur ea laboris dolor nisi id sunt aute. Duis tempor ex exercitation enim nulla ea reprehenderit id cupidatat esse.',
-            picture: null
-        }
-    ]
+    entries:[]
 })
