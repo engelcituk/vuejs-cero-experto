dc005baefeec11f4e245a58dd0ca254d1f2edcea
cituk <programaciondyn3.mx@sandos.com>
2021-09-12 00:38:44 -0500

    Borrar archivos subidos en las pruebas

--- 07-journal-app/tests/unit/modules/daybook/helpers/uploadImage.spec.js
+++ 07-journal-app/tests/unit/modules/daybook/helpers/uploadImage.spec.js
@@ -1,10 +1,17 @@
 
+import cloudinary from 'cloudinary'
 import uploadImage from '@/modules/daybook/helpers/uploadImage'
 import axios from 'axios'
 
+cloudinary.config({
+    cloud_name: 'di3qzyeke',
+    api_key: '786616524812658',
+    api_secret:'Ow80aa6Bv0VsjGgbsuHfEE1VVYQ'
+})
+
 describe('Pruebas en el UploadImage', ()=> {
     
-    test('Debe de cargar un∫ archivo y retornar el url',  async () => { 
+    test('Debe de cargar un archivo y retornar el url',  async (done) => { 
         const { data } = await axios.get(`https://res.cloudinary.com/di3qzyeke/image/upload/v1606757933/sample.jpg`,{
             responseType: 'arraybuffer'
         })
@@ -13,6 +20,14 @@ describe('Pruebas en el UploadImage', ()=> {
         const url = await uploadImage( file )
         // console.log(url)
         expect( typeof url ).toBe('string')
+
+        // tomar el id
+        const segments = url.split('/')
+        const imageId = segments[segments.length -1 ].replace('.jpg','')
+        cloudinary.v2.api.delete_resources(imageId, {},()=>{
+            done()
+        })
+        console.log(imageId)
         
     })  
   
