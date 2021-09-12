5c8a624927b3dca5cbf4cbb1eec2b4a652b19d8f
cituk <programaciondyn3.mx@sandos.com>
2021-09-11 23:11:16 -0500

    Tarea - Pruebas en el FAB Component

--- 07-journal-app/src/modules/daybook/components/Fab.vue
+++ 07-journal-app/src/modules/daybook/components/Fab.vue
@@ -11,7 +11,7 @@
             icon:{
                 type: String,
                 default: 'fa-plus',
-                required: true
+                // required: true
             }
         },
         computed:{
---
+++ 07-journal-app/tests/unit/modules/daybook/components/fab.spec.js
@@ -0,0 +1,30 @@
+
+import { shallowMount } from '@vue/test-utils'
+import Fab from '@/modules/daybook/components/Fab.vue'
+
+describe('Pruebas en el Fab component', ()=> {
+    
+    test('Debe de mostrar el icono por defecto fa-plus', ()=> {
+        const wrapper = shallowMount( Fab )
+        const iTag = wrapper.find('i')
+        expect( iTag.classes('fa-plus') ).toBeTruthy()
+    })   
+    
+    test('Debe de mostrar el icono por argumento :fa-save', ()=> {
+        const wrapper = shallowMount( Fab,{
+            props:{
+                icon: 'fa-save'
+            }
+        })
+        const iTag = wrapper.find('i')
+        expect( iTag.classes('fa-save') ).toBeTruthy()    
+    })
+    
+    test('Debe de emitir el evento save-entry cuando se hace click en btn', ()=> {
+        const wrapper = shallowMount( Fab )      
+        wrapper.find('button').trigger('click')
+        expect( wrapper.emitted('save-entry') ).toHaveLength(1)    
+    }) 
+
+  
+})
