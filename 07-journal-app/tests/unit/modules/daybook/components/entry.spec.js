f1eb7b908753be798cb57e8ebbae3bb864d51439
cituk <programaciondyn3.mx@sandos.com>
2021-09-12 22:42:24 -0500

    Solución de la tarea - EntryComponent

--- 07-journal-app/tests/unit/modules/daybook/components/entry.spec.js
+++ 07-journal-app/tests/unit/modules/daybook/components/entry.spec.js
@@ -1,21 +1,47 @@
 
 import { shallowMount } from '@vue/test-utils'
-import Entry from '@/modules/daybook/components/Entry'
+import Entry from '@/modules/daybook/components/Entry.vue'
+import { journalState } from './../../../mock-data/test-journal-state'
 
-describe('Pruebas en el Entry component', ()=> {
-    
+describe('Pruebas en el Entry component', () => {
+
+    const mockRouter = {
+        push: jest.fn()
+    }
+
+    const wrapper = shallowMount( Entry, {
+        props:{
+            entry: journalState.entries[0]
+        },
+        global:{
+            mocks:{
+                $router: mockRouter
+            }
+        }
+    })
     test('Debe de hacer match con el snapshot', ()=> {
-        const wrapper = shallowMount( Entry )
+        expect( wrapper.html() ).toMatchSnapshot()
         
     })   
       
     test('Debe de redireccionar al hacer click en el entry-container', ()=> {
-        const wrapper = shallowMount( Entry )
-        
+        const entryContainer = wrapper.find('.entry-container')
+
+        entryContainer.trigger('click')
+
+        expect( mockRouter.push ).toHaveBeenCalled()
+        expect( mockRouter.push ).toHaveBeenCalledWith({
+            name:'entry',
+            params:{
+                id: journalState.entries[0].id // expect
+            }
+        })
+
     }) 
 
     test('Pruebas en las propiedades computadas', ()=> {
-        const wrapper = shallowMount( Entry )
-        
+        expect( wrapper.vm.day ).toBe(11)
+        expect( wrapper.vm.month ).toBe('Septiembre')
+        expect( wrapper.vm.yearDay ).toBe('2021, Sábado')        
     }) 
 })
