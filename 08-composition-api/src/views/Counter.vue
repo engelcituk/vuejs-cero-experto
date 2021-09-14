0635ff0a5c8722aabc1a9bcc6e1f247b8df78e49
cituk <programaciondyn3.mx@sandos.com>
2021-09-13 23:08:25 -0500

    Counter usando composition API

--- 08-composition-api/src/views/Counter.vue
+++ 08-composition-api/src/views/Counter.vue
@@ -1,15 +1,44 @@
 <template>
     <div>
         <h1>Counter vue</h1>
+        <h2>{{counter}}</h2>
+        <button @click="decrease">-1</button>
+        <button @click="increase">+1</button>
+
     </div>
 </template>
-
 <script>
+    import { ref } from 'vue' 
+
     export default {
-        
+        name:'Counter',
+        props:{},
+        emits:[],
+        setup () {
+
+            const counter = ref(5)
+            
+            /*
+            innecesario
+            const increase = () => {
+                counter.value ++
+            }
+
+            const decrease = () => {
+                counter.value --
+            }
+            */
+            return {
+                //objetos reactivos
+                counter,
+                //métodos
+                decrease: () => counter.value --,
+                increase: () => counter.value ++,
+            }
+        }
     }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
