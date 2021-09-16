24f37b3a659a842a443526e017254ab0a05225f9
cituk <programaciondyn3.mx@sandos.com>
2021-09-16 16:16:08 -0500

    Rutas de Login y Register

--- 07-journal-app/src/modules/auth/router/index.js
+++ 07-journal-app/src/modules/auth/router/index.js
@@ -2,20 +2,16 @@ export default {
     name: 'auth',
     component: () => import(/* webpackChunkName: "Auth" */ '@/modules/auth/layouts/AuthLayout'),
     children:[
-        // {
-        //     path:'',
-        //     name:'no-entry',
-        //     component: () => import(/* webpackChunkName: "noEntryView" */ '@/modules/daybook/views/NoEntrySelected'),
-        // },
-        // {
-        //     path:':id',
-        //     name:'entry',
-        //     component: () => import(/* webpackChunkName: "noEntryView" */ '@/modules/daybook/views/EntryView'),
-        //     props: ( route ) => {
-        //         return {
-        //             id: route.params.id
-        //         }
-        //     } 
+        {
+            path:'',
+            name:'login',
+            component: () => import(/* webpackChunkName: "LoginView" */ '@/modules/auth/views/Login'),
+        },
+        {
+            path:'register',
+            name:'register',
+            component: () => import(/* webpackChunkName: "RegisterView" */ '@/modules/auth/views/Register'),
+        }    
         // }
 
     ]
