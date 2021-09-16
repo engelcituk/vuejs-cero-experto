60184777475c82242e6f3c00586e0dc0113902ae
cituk <programaciondyn3.mx@sandos.com>
2021-09-16 11:24:41 -0500

    Diseño del State

--- 08-composition-api/src/store/index.js
+++ 08-composition-api/src/store/index.js
@@ -2,6 +2,13 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    todos:[
+      {id:'1', text:'Recolectar las piedras del infinito', completed: false},
+      {id:'2', text:'Piedra del alma', completed: true},
+      {id:'3', text:'Piedra del poder', completed: true},
+      {id:'4', text:'Piedra de la realida', completed: false},
+      {id:'5', text:'Conseguir nuevos secuaces competentes', completed: false},
+    ]
   },
   mutations: {
   },
--- 08-composition-api/src/views/TodoVuex.vue
+++ 08-composition-api/src/views/TodoVuex.vue
@@ -1,6 +1,8 @@
+
 <template>
     <div>
         <h1>Thanos todo List</h1>
+        <h4>Tareas: {{$store.state.todos.length}}</h4>
     </div>
 </template>
 
