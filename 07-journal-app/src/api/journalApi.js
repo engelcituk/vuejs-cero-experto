10fe592d05c3d41ebe950771bb58887042edb065
cituk <programaciondyn3.mx@sandos.com>
2021-09-17 02:10:34 -0500

    Interceptores

--- 07-journal-app/src/api/journalApi.js
+++ 07-journal-app/src/api/journalApi.js
@@ -5,6 +5,13 @@ const journalApi = axios.create({
     baseURL: `https://crud-vue-13645.firebaseio.com`
 })
 
+//intercepetor para establecer credenciales, autorizaciones.. etc
+journalApi.interceptors.request.use( (config) => {
+    config.params = {
+        auth: localStorage.getItem('idToken')
+    }
+    return config
+})
 // console.log(process.env.NODE_ENV)
 
 export default journalApi
