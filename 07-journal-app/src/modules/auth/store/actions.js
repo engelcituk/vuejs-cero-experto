700ba93f023193d514fc35946329c4aa0bf8282c
cituk <programaciondyn3.mx@sandos.com>
2021-09-17 00:07:30 -0500

    Actualizar el displayName del usuario

--- 07-journal-app/src/modules/auth/store/actions.js
+++ 07-journal-app/src/modules/auth/store/actions.js
@@ -3,14 +3,17 @@ import authApi from '@/api/authApi'
 
 export const createUser = async ({commit}, user) => {
 
-    const { email, password } = user
+    const { email, password, name } = user
 
     try {
 
         const { data } = await authApi.post(`:signUp`, {email, password, returnSecureToken: true})
         console.log(data)
         console.log(commit)
+        const { idToken,  } = data
 
+        const resp = await authApi.post(`:update`, {displayName: name, idToken})
+        console.log(resp)
         return { ok: true, }
 
     } catch (error) {
