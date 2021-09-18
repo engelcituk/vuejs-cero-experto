b1e6e5768dffaea53cb4b14de4e523f2a06a30c4
cituk <programaciondyn3.mx@sandos.com>
2021-09-18 00:38:16 -0500

    Mutations - loginUser

--- 07-journal-app/tests/unit/modules/auth/store/auth-module.spec.js
+++ 07-journal-app/tests/unit/modules/auth/store/auth-module.spec.js
@@ -16,6 +16,30 @@ describe('Vuex - Pruebas en el Auth module', () => {
         expect(user).toBe(null)
         expect(idToken).toBe(null)
         expect(refreshToken).toBe(null)
-
     })  
+    //mutations
+    test('Mutation: loginUser', () => { 
+        
+        const store = createVuexStore({
+            status: 'authenticating', // 'authenticated' , 'not-authenticated', 'authenticating'
+            user: null,
+            idToken: null,
+            refreshToken: null
+        })
+
+        const payload = {
+            user : { name: 'Jonas', email:'jonas@mail.com' },
+            idToken: 'ABC-123',
+            refreshToken: 'XYZ'
+        }
+
+        store.commit('auth/loginUser', payload )
+
+        const { status, user, idToken, refreshToken } = store.state.auth
+        expect(status).toBe('authenticated')
+        expect(user).toEqual({ name: 'Jonas', email:'jonas@mail.com' })
+        expect(idToken).toBe('ABC-123')
+        expect(refreshToken).toBe('XYZ')
+    }) 
+
 })
