3efa800812532f05b08caff772b95d5b01f02964
cituk <programaciondyn3.mx@sandos.com>
2021-09-20 02:01:01 -0500

    Formulario con credenciales correctas

--- 07-journal-app/tests/unit/modules/auth/views/login.spec.js
+++ 07-journal-app/tests/unit/modules/auth/views/login.spec.js
@@ -54,7 +54,7 @@ describe('Pruebas en el View Login', () => {
 
         store.dispatch.mockReturnValueOnce( {ok: true, message: 'Error con las credenciales' } )
 
-        const wrapper = shallowMount(Login, {
+        const wrapper = shallowMount( Login, {
             global: {
                 plugins:[ store ]
             }
@@ -63,7 +63,27 @@ describe('Pruebas en el View Login', () => {
 
         expect( store.dispatch ).toHaveBeenCalledWith('auth/signInUser', { email: '', password: ''})
         // expect( Swal.fire ).toHaveBeenCalledWith( "Error", "Error con las credenciales", "error" )
+    })
+
+    test('Credenciales correctas, debe de redirigir a la ruta no-entry ', async () => { 
+
+        store.dispatch.mockReturnValueOnce( {ok: true } )
+
+        const wrapper = shallowMount( Login, {
+            global: {
+                plugins:[ store ]
+            }
+        })
+        
+        const [ txtEmail, txtPassword ] = wrapper.findAll('input')
+        await txtEmail.setValue('juanit@mail.com')
+        await txtPassword.setValue('12425252523')
+
+        await wrapper.find('form').trigger('submit')
+
+        expect( store.dispatch ).toHaveBeenCalledWith('auth/signInUser', { email: 'juanit@mail.com', password: '12425252523'})        
+        expect( wrapper.router.push ).toHaveBeenCalledWith({ name:'no-entry' })        
 
     })
    
-})
+}) 
