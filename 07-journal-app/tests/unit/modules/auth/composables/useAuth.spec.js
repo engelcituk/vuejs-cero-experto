86583df116d0975557136e70b388f34284cec4d0
cituk <programaciondyn3.mx@sandos.com>
2021-09-19 23:50:10 -0500

    Login exitoso y login fallido

--- 07-journal-app/tests/unit/modules/auth/composables/useAuth.spec.js
+++ 07-journal-app/tests/unit/modules/auth/composables/useAuth.spec.js
@@ -30,6 +30,35 @@ describe('Pruebas en el useAuth', () => {
         const respuesta = await createUser( newUser )
         expect( mockStore.dispatch ).toHaveBeenCalledWith("auth/createUser", newUser)
         expect( respuesta ).toEqual({ ok: false, message: 'EMAIL_EXISTS' })
+    })
+    
+    test('Login Exitoso', async () => { 
+        const { loginUser } = useAuth()
+        const loginForm = { email:'jonas@mail.com', password:'123456'}
+        mockStore.dispatch.mockReturnValue({ ok: true })
+
+        const respuesta = await loginUser( loginForm )
+        expect( mockStore.dispatch ).toHaveBeenCalledWith("auth/signInUser", loginForm)
+        expect( respuesta ).toEqual({ ok: true})
     }) 
+
+    test('Login Fallido', async () => { 
+        const { loginUser } = useAuth()
+        const loginForm = { email:'jonas@mail.com', password:'123456'}
+        mockStore.dispatch.mockReturnValue({ ok: false, message:'EMAIL/PASSWORD do not exist'})
+
+        const respuesta = await loginUser( loginForm )
+        expect( mockStore.dispatch ).toHaveBeenCalledWith("auth/signInUser", loginForm)
+        expect( respuesta ).toEqual({ ok: false, message:'EMAIL/PASSWORD do not exist'})
+    }) 
+
+    test('checkAuthStatus', async () => { 
+        const { checkAuthStatus } = useAuth()
+        mockStore.dispatch.mockReturnValue({ ok: true})
+
+        const respuesta = await checkAuthStatus()
+        expect( mockStore.dispatch ).toHaveBeenCalledWith("auth/checkAuthentication")
+        expect( respuesta ).toEqual({ ok: true})
+    })
    
 })
