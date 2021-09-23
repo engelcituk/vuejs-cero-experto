d0ec20f62010222dcbc208e7455fd6766b052d88
cituk <programaciondyn3.mx@sandos.com>
2021-09-22 23:04:51 -0500

    Cambiar tamaño dependiendo de la pantalla

--- 09-quasar/src/pages/Flex.vue
+++ 09-quasar/src/pages/Flex.vue
@@ -14,19 +14,27 @@
     </div>
 
     <div class="row q-mb-md justify-center">
+      <div class="col col-4 bg-primary"></div>
       <div class="col col-4 bg-secondary"></div>
-      <div class="col col-4 bg-accent"></div>
     </div>
 
     <div class="row q-mb-md justify-end">
+      <div class="col col-4 bg-primary"></div>
       <div class="col col-4 bg-secondary"></div>
-      <div class="col col-4 bg-accent"></div>
     </div>
 
     <div class="row q-mb-md justify-between">
+      <div class="col col-4 bg-primary"></div>
       <div class="col col-4 bg-secondary"></div>
-      <div class="col col-4 bg-accent"></div>
     </div>
+
+    <q-separator spaced/>
+
+    <div class="row q-mb-md justify-between">
+      <div class="col-12 col-sm-6 col-md-4 bg-primary"></div>
+      <div class="col-12 col-sm-6 col-md-4 bg-secondary"></div>
+    </div>
+
   </q-page>
 </template>
 
